7ca389654ddf5ddd8620ed3c046943be3e0c5d50
Chris1234 <131827823+ChrisAldous@users.noreply.github.com>
2025-07-30 16:29:50 -0600

    Added intro screen following tutorials lead

---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Welcome Home"),),
+      body: Center(child: Text("Welcome", style: TextStyle(fontSize: 36),)),
+    );
+  }
+}
