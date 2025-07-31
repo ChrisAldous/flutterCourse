fb4622a1a4a71f877b2f2d9c559cdf2a2510ceff
Chris1234 <131827823+ChrisAldous@users.noreply.github.com>
2025-07-31 14:34:01 -0600

    added an assets file, changed the pubspec.yaml to allow for assets file and a background image.

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -7,7 +7,30 @@ class IntroScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Welcome Home"),),
-      body: Center(child: Text("Welcome", style: TextStyle(fontSize: 36),)),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset("assets/Beach.jpg", fit: BoxFit.cover),),
+          Align(
+            alignment: Alignment(0, -0.5),
+            child: const Text("Welcome",
+             style: TextStyle(
+              color: Colors.white,
+              shadows: [Shadow(
+                color: Colors.black,
+                blurRadius: 10,
+                offset: Offset(5, 5)
+              )],
+              fontSize: 36))
+          ),
+          Align(
+            alignment: Alignment(0, 0.5),
+            child: ElevatedButton(
+              onPressed: (){},
+              child: const Text("Select")
+            ),
+          )
+          ]),
     );
   }
 }
