b5ffdd778fb43cc87593bbef8ffe8d8ad2512804
Chris1234 <131827823+ChrisAldous@users.noreply.github.com>
2025-08-06 18:03:24 -0600

    Added images to let user change the IntroScreen Background. Added DropdownButton with List of items. Currently the Changes made by user on DropdownButton will not reflect on the UI.

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -9,23 +9,34 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
   final TextEditingController txtName = TextEditingController();
+  final List<String> _images = ['Beach', 'Forest', 'Lake', 'Mountain'];
+  String _selectedImage = 'Beach';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Settings")),
-      body: 
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: txtName,
-                decoration: InputDecoration(
-                  hintText: "Enter Your name"
-                  ),
-              )
-            ]),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: txtName,
+              decoration: InputDecoration(hintText: "Enter Your name"),
+            ),
+            DropdownButton(
+              value: _selectedImage,
+              // icon: const Icon(Icons.arrow_downward_outlined),
+              items: _images.map((String value) {
+                return DropdownMenuItem(value: value, child: Text(value));
+              }).toList(),
+              onChanged: (newvalue) {
+                _selectedImage = newvalue ?? 'Beach';
+              },
+            ),
+          ],
         ),
+      ),
     );
   }
 }
